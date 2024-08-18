69cf00f9fdfd0c2a05479c759ed905b08c71d371
Your Name <manzi5000@hotmail.co.uk>
2024-08-18 15:52:22 +0100

    added character functional

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -2,44 +2,84 @@ import React, { Component } from "react";
 import Name from "./Name";
 import Quote from "./Quote";
 import Image from "./Image";
+import Controls from "./CharacterControls";
 
-class Character extends Component {
-  state = { liked: false };
-  //test
-  //test
-  //test
-  onLikeToggle = () => {
-    this.setState({ liked: !this.state.liked });
-  };
-
-  render() {
-    const { name, quote, image, characterDirection } = this.props;
-    const { liked } = this.state;
-
-    if (characterDirection === "Left") {
-      return (
-        <div
-          onClick={() => this.onLikeToggle("Hello")}
-          className={liked ? "character liked" : "character notLiked"}
-        >
-          <Name name={name} />
-          <Image imageUrl={image} imageAlt={name} />
-          <Quote quote={quote} />
-        </div>
-      );
-    }
-
+function Character({
+  name,
+  quote,
+  image,
+  characterDirection,
+  id,
+  liked,
+  onLikeToggle,
+  onDelete,
+}) {
+  if (characterDirection === "Left") {
     return (
-      <div
-        className={liked ? "character liked" : "character notLiked"}
-        onClick={this.onLikeToggle}
-      >
+      <div>
         <Name name={name} />
-        <Quote quote={quote} />
         <Image imageUrl={image} imageAlt={name} />
+        <Quote quote={quote} />
+        <Controls
+          id={id}
+          liked={liked}
+          onLikeToggle={onLikeToggle}
+          onDelete={onDelete}
+        />
       </div>
     );
   }
+
+  return (
+    <div>
+      <Name name={name} />
+      <Quote quote={quote} />
+      <Image imageUrl={image} imageAlt={name} />
+      <Controls
+        id={id}
+        liked={liked}
+        onLikeToggle={onLikeToggle}
+        onDelete={onDelete}
+      />
+    </div>
+  );
 }
+
+// class Character extends Component {
+//   state = { liked: false };
+
+//   onLikeToggle = () => {
+//     this.setState({ liked: !this.state.liked });
+//   };
+
+//   render() {
+//     const { name, quote, image, characterDirection } = this.props;
+//     const { liked } = this.state;
+
+//     if (characterDirection === "Left") {
+//       return (
+//         <div
+//           onClick={() => this.onLikeToggle("Hello")}
+//           className={liked ? "character liked" : "character notLiked"}
+//         >
+//           <Name name={name} />
+//           <Image imageUrl={image} imageAlt={name} />
+//           <Quote quote={quote} />
+//         </div>
+//       );
+//     }
+
+//     return (
+//       <div
+//         className={liked ? "character liked" : "character notLiked"}
+//         onClick={this.onLikeToggle}
+//       >
+//         <Name name={name} />
+//         <Quote quote={quote} />
+//         <Image imageUrl={image} imageAlt={name} />
+//       </div>
+//     );
+//   }
+// }
 
 export default Character;
