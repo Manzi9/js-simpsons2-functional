7caca7a1a54a8ee2b5596d41507ceb93fbcf990d
Your Name <manzi5000@hotmail.co.uk>
2024-11-15 12:45:44 +0000

    char function-based + likeBTN css working

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -4,79 +4,79 @@ import Quote from "./Quote";
 import Image from "./Image";
 import CharacterControls from "./CharacterControls";
 
-// function Character({
-//   name,
-//   quote,
-//   image,
-//   characterDirection,
-//   id,
-//   liked,
-//   onLikeToggle,
-//   onDelete,
-// }) {
-//   if (characterDirection === "Left") {
-//     return (
-//       //ID makes styling easier
-//       <div
-//         onClick={onLikeToggle}
-//         class={liked ? "character liked" : "character not liked"}
-//       >
-//         <Name name={name} />
-//         <Image imageUrl={image} imageAlt={name} />
-//         <Quote quote={quote} />
-//         <Controls
-//           id={id}
-//           liked={liked}
-//           onLikeToggle={onLikeToggle}
-//           onDelete={onDelete}
-//         />
-//       </div>
-//     );
-//   }
-
-//   return (
-//     <div
-//       onClick={onLikeToggle}
-//       class={liked ? "character liked" : "character not liked"}
-//     >
-//       <Name name={name} />
-//       <Quote quote={quote} />
-//       <Image imageUrl={image} imageAlt={name} />
-//       <Controls
-//         id={id}
-//         liked={liked}
-//         onLikeToggle={onLikeToggle}
-//         onDelete={onDelete}
-//       />
-//     </div>
-//   );
-// }
-
-class Character extends Component {
-  render() {
-    const { name, quote, image, characterDirection, id, liked, onLikeToggle } =
-      this.props;
-
-    if (characterDirection === "Left") {
-      return (
-        <div>
-          <CharacterControls id={id} like={liked} onLikeToggle={onLikeToggle} />
-          <Name name={name} />
-          <Image imageUrl={image} imageAlt={name} />
-          <Quote quote={quote} />
-        </div>
-      );
-    }
-
+function Character({
+  name,
+  quote,
+  image,
+  characterDirection,
+  id,
+  liked,
+  onLikeToggle,
+  onDelete,
+}) {
+  if (characterDirection === "Left") {
     return (
-      <div>
-        <CharacterControls id={id} like={liked} onLikeToggle={onLikeToggle} />
+      //ID makes styling easier
+      <div
+        onClick={onLikeToggle}
+        class={liked ? "character liked" : "character notLiked"}
+      >
         <Name name={name} />
-        <Quote quote={quote} />
         <Image imageUrl={image} imageAlt={name} />
+        <Quote quote={quote} />
+        <CharacterControls
+          id={id}
+          liked={liked}
+          onLikeToggle={onLikeToggle}
+          onDelete={onDelete}
+        />
       </div>
     );
   }
+
+  return (
+    <div
+      onClick={onLikeToggle}
+      class={liked ? "character liked" : "character notLiked"}
+    >
+      <Name name={name} />
+      <Quote quote={quote} />
+      <Image imageUrl={image} imageAlt={name} />
+      <CharacterControls
+        id={id}
+        liked={liked}
+        onLikeToggle={onLikeToggle}
+        onDelete={onDelete}
+      />
+    </div>
+  );
 }
+
+// class Character extends Component {
+//   render() {
+//     const { name, quote, image, characterDirection, id, liked, onLikeToggle } =
+//       this.props;
+
+//     if (characterDirection === "Left") {
+//       return (
+//         <div>
+//           <CharacterControls id={id} like={liked} onLikeToggle={onLikeToggle} />
+//           <Name name={name} />
+//           <Image imageUrl={image} imageAlt={name} />
+//           <Quote quote={quote} />
+//         </div>
+//       );
+//     }
+
+//     return (
+//       <div>
+//         <CharacterControls id={id} like={liked} onLikeToggle={onLikeToggle} />
+//         <Name name={name} />
+//         <Quote quote={quote} />
+//         <Image imageUrl={image} imageAlt={name} />
+//       </div>
+//     );
+//   }
+// }
 
 export default Character;
